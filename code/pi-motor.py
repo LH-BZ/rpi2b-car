7cda377635b99992fba399f9fe252e6a2c0aed02
simbasailor <simba_sailor@163.com>
2016-03-12 00:39:19 +0800

    can work, but not very well

--- code/pi-motor.py
+++ code/pi-motor.py
@@ -15,58 +15,53 @@ class motor():
     version = '0.01'
     speed = [0, 0]
 
-def __init__(self, LeftPin=[18, 23], RightPin=[24, 25]):
-    self.left  = LeftPin
-    self.right = RightPin
-    GPIO.setmode(GPIO.BCM)
-    for pin in (self.left + self.right):
-        GPIO.setup(pin, GPIO.OUT)
-    self.left_pwm  = [GPIO.PWM(self.left[0],  100), GPIO.PWM(self.left[1],  100)]
-    self.right_pwm = [GPIO.PWM(self.right[0], 100), GPIO.PWM(self.right[1], 100)]
+    def __init__(self, LeftPin=[18, 23], RightPin=[24, 25]):
+        self.left  = LeftPin
+        self.right = RightPin
+        GPIO.setmode(GPIO.BCM)
+        for pin in (self.left + self.right):
+            GPIO.setup(pin, GPIO.OUT)
+        self.left_pwm  = [GPIO.PWM(self.left[0],  100), GPIO.PWM(self.left[1],  100)]
+        self.right_pwm = [GPIO.PWM(self.right[0], 100), GPIO.PWM(self.right[1], 100)]
 
-def start(self):
-    left_speed = self.speed[0]
-    right_speed = self.speed[1]
-    if left_speed > 0 :
-        self.left_pwm[0].start(left_speed)
-        self.left_pwm[1].start(0)
-    else:
-        self.left_pwm[0].start(0)
-        self.left_pwm[1].start(-left_speed)
-    if right_speed > 0 :
-        self.right_pwm[0].start(right_speed)
-        self.right_pwm[1].start(0)
-    else:
-        self.right_pwm[0].start(0)
-        self.right_pwm[1].start(-left_speed)
+    def start(self):
+        left_speed = self.speed[0]
+        right_speed = self.speed[1]
+        if left_speed > 0 :
+            self.left_pwm[0].start(left_speed)
+            self.left_pwm[1].start(0)
+        else:
+            self.left_pwm[0].start(0)
+            self.left_pwm[1].start(-left_speed)
+        if right_speed > 0 :
+            self.right_pwm[0].start(right_speed)
+            self.right_pwm[1].start(0)
+        else:
+            self.right_pwm[0].start(0)
+            self.right_pwm[1].start(-right_speed)
 
-def stop(self):
-    self.left_pwm[0].stop()
-    self.left_pwm[1].stop()
-    self.right_pwm[0].stop()
-    self.right_pwm[1].stop()
+    def stop(self):
+        self.left_pwm[0].stop()
+        self.left_pwm[1].stop()
+        self.right_pwm[0].stop()
+        self.right_pwm[1].stop()
  
 
-# speed: 0 ~ 100 
-def run_speed(self, left_speed, right_speed):
-    self.speed[0] = left_speed 
-    self.speed[1] = right_speed
+    # speed: 0 ~ 100 
+    def run_speed(self, left_speed, right_speed):
+        self.speed[0] = left_speed 
+        self.speed[1] = right_speed
     
-    if left_speed > 0 :
-        self.left_pwm[0].ChangeDutyCycle(left_speed)
-        self.left_pwm[1].ChangeDutyCycle(0)
-    else:
-        self.left_pwm[0].ChangeDutyCycle(0)
-        self.left_pwm[1].ChangeDutyCycle(-left_speed)
-    if right_speed > 0 :
-        self.right_pwm[0].ChangeDutyCycle(right_speed)
-        self.right_pwm[1].ChangeDutyCycle(0)
-    else:
-        self.right_pwm[0].ChangeDutyCycle(0)
-        self.right_pwm[1].ChangeDutyCycle(-left_speed)
-
-
-def stand_turn():
-
-
+        if left_speed > 0 :
+            self.left_pwm[0].ChangeDutyCycle(left_speed)
+            self.left_pwm[1].ChangeDutyCycle(0)
+        else:
+            self.left_pwm[0].ChangeDutyCycle(0)
+            self.left_pwm[1].ChangeDutyCycle(-left_speed)
+        if right_speed > 0 :
+            self.right_pwm[0].ChangeDutyCycle(right_speed)
+            self.right_pwm[1].ChangeDutyCycle(0)
+        else:
+            self.right_pwm[0].ChangeDutyCycle(0)
+            self.right_pwm[1].ChangeDutyCycle(-right_speed)
 
